e1dba2b349fca6af2c8136a3cbccde7eeb13321c
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2026-08-30 13:21:37 +0400

    gateway: csrf disable

--- src/main/java/com/example/gwbranch/config/SecurityConfig.java
+++ src/main/java/com/example/gwbranch/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.gwbranch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,6 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/auth/login/**").permitAll()
